3dd172acdf906509cfcef37e251d03382d2b1fe5
khavi <vib2105566@student.ctu.edu.vn>
2025-03-21 00:54:01 +0700

    Dinh nghia cac route cho tai nguyen Book

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 
+const booksRouter = require("./app/routes/book.route");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/books", booksRouter);
 
 app.get("/", (req, res) => {
     res.json({message: "Chào mừng đến ứng dụng quản lý mượn sách."});
